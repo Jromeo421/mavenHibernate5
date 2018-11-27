1b50f899f7717928a4845bdcf85687c1b62eb297
JJ Hugh <hug17010@byui.edu>
2018-11-27 13:34:55 -0600

    Hibernate updates for the current code

--- src/main/java/com/hibernatetake5/mavenhibernate5/App.java
+++ src/main/java/com/hibernatetake5/mavenhibernate5/App.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
 /**
@@ -26,9 +27,8 @@ public class App {
        
        Configuration con = new Configuration().configure().addAnnotatedClass(Clients.class);
        
-        ServiceRegistry reg = new ServiceRegistryBuilder().applySetting(con.getProperties()).buildServiceRegistry();
+        ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
                 
-        
         SessionFactory sf = con.buildSessionFactory(reg);
         
         Session session = sf.openSession();
