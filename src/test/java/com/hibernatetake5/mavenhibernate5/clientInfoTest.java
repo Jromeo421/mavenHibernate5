118f6cb5be4fa10ed6b9531f16955c22dac90574
JJ Hugh <hug17010@byui.edu>
2018-11-28 13:41:17 -0600

    Updated Portfolia FIles

---
+++ src/test/java/com/hibernatetake5/mavenhibernate5/clientInfoTest.java
@@ -0,0 +1,260 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hibernatetake5.mavenhibernate5;
+
+import org.junit.After;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author JRome
+ */
+public class clientInfoTest {
+    
+    public clientInfoTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getClientid method, of class clientInfo.
+     */
+    @Test
+    public void testGetClientid() {
+        System.out.println("getClientid");
+        clientInfo instance = new clientInfo();
+        int expResult = 0;
+        int result = instance.getClientid();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setClientid method, of class clientInfo.
+     */
+    @Test
+    public void testSetClientid() {
+        System.out.println("setClientid");
+        int Clientid = 0;
+        clientInfo instance = new clientInfo();
+        instance.setClientid(Clientid);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getFullName method, of class clientInfo.
+     */
+    @Test
+    public void testGetFullName() {
+        System.out.println("getFullName");
+        clientInfo instance = new clientInfo();
+        String expResult = "";
+        String result = instance.getFullName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setFullName method, of class clientInfo.
+     */
+    @Test
+    public void testSetFullName() {
+        System.out.println("setFullName");
+        String fullName = "";
+        clientInfo instance = new clientInfo();
+        instance.setFullName(fullName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPhoneNumber method, of class clientInfo.
+     */
+    @Test
+    public void testGetPhoneNumber() {
+        System.out.println("getPhoneNumber");
+        clientInfo instance = new clientInfo();
+        String expResult = "";
+        String result = instance.getPhoneNumber();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPhoneNumber method, of class clientInfo.
+     */
+    @Test
+    public void testSetPhoneNumber() {
+        System.out.println("setPhoneNumber");
+        String phoneNumber = "";
+        clientInfo instance = new clientInfo();
+        instance.setPhoneNumber(phoneNumber);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAppointment_date method, of class clientInfo.
+     */
+    @Test
+    public void testGetAppointment_date() {
+        System.out.println("getAppointment_date");
+        clientInfo instance = new clientInfo();
+        String expResult = "";
+        String result = instance.getAppointment_date();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setAppointment_date method, of class clientInfo.
+     */
+    @Test
+    public void testSetAppointment_date() {
+        System.out.println("setAppointment_date");
+        String appointment_date = "";
+        clientInfo instance = new clientInfo();
+        instance.setAppointment_date(appointment_date);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPrice method, of class clientInfo.
+     */
+    @Test
+    public void testGetPrice() {
+        System.out.println("getPrice");
+        clientInfo instance = new clientInfo();
+        int expResult = 0;
+        int result = instance.getServicePrice();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPrice method, of class clientInfo.
+     */
+    @Test
+    public void testSetPrice() {
+        System.out.println("setPrice");
+        int price = 0;
+        clientInfo instance = new clientInfo();
+        instance.setServicePrice(price);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDiscounts method, of class clientInfo.
+     */
+    @Test
+    public void testGetDiscounts() {
+        System.out.println("getDiscounts");
+        clientInfo instance = new clientInfo();
+        String expResult = "";
+        String result = instance.getDiscounts();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setDiscounts method, of class clientInfo.
+     */
+    @Test
+    public void testSetDiscounts() {
+        System.out.println("setDiscounts");
+        String discounts = "";
+        clientInfo instance = new clientInfo();
+        instance.setDiscounts(discounts);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEmail_address method, of class clientInfo.
+     */
+    @Test
+    public void testGetEmail_address() {
+        System.out.println("getEmail_address");
+        clientInfo instance = new clientInfo();
+        String expResult = "";
+        String result = instance.getEmail_address();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEmail_address method, of class clientInfo.
+     */
+    @Test
+    public void testSetEmail_address() {
+        System.out.println("setEmail_address");
+        String email_address = "";
+        clientInfo instance = new clientInfo();
+        instance.setEmail_address(email_address);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDate_of_birth method, of class clientInfo.
+     */
+    @Test
+    public void testGetDate_of_birth() {
+        System.out.println("getDate_of_birth");
+        clientInfo instance = new clientInfo();
+        String expResult = "";
+        String result = instance.getDate_of_birth();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setDate_of_birth method, of class clientInfo.
+     */
+    @Test
+    public void testSetDate_of_birth() {
+        System.out.println("setDate_of_birth");
+        String date_of_birth = "";
+        clientInfo instance = new clientInfo();
+        instance.setDate_of_birth(date_of_birth);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class clientInfo.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        clientInfo instance = new clientInfo();
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
